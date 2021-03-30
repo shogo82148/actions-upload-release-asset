86d946dc9830857e74fdecfd62b7b873abf3c642
Ichinose Shogo <shogo82148@gmail.com>
2021-03-30 21:51:59 +0900

    bump up to v1.2.5

--- dist/index.js
+++ dist/index.js
@@ -2809,7 +2809,9 @@ class HttpClient {
                 maxSockets: maxSockets,
                 keepAlive: this._keepAlive,
                 proxy: {
-                    proxyAuth: `${proxyUrl.username}:${proxyUrl.password}`,
+                    ...((proxyUrl.username || proxyUrl.password) && {
+                        proxyAuth: `${proxyUrl.username}:${proxyUrl.password}`
+                    }),
                     host: proxyUrl.hostname,
                     port: proxyUrl.port
                 }
