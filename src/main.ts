33a9352bb88e448f1636655ea667481d7371bfaa
Ichinose Shogo <shogo82148@gmail.com>
2021-08-30 20:47:12 +0900

    fix compile error

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,11 @@ async function run(): Promise<void> {
     })
     core.setOutput('browser_download_url', output.browser_download_url)
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error)
+    } else {
+      core.setFailed(`${error}`)
+    }
   }
 }
 
